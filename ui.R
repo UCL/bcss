abf6c8489530285b5ef9bc9b8c3c73335dc6d908
KMcgrathUCL <45361270+KMcgrathUCL@users.noreply.github.com>
2019-02-04 13:39:35 +0000

    edits form email

--- ui.R
+++ ui.R
@@ -7,6 +7,8 @@
 #test git
 #NO tabs
 #x axis proportion
+# try to give user option of using 2 scales for ICC  0-1 or 0-0.1, easier than logify scale.
+
 
 #https://stackoverflow.com/questions/32969659/shiny-reactive-ggplot-output
 #for ggplot
@@ -19,26 +21,31 @@ library(plotly)
 library(shinyjs)
 ui <- fluidPage(
   titlePanel(title = h4("R shiny app to accompany \"Cluster randomised trials with baseline data:sample size and optimal designs\" ,Copas and Hooper")),
-  sidebarPanel(
-    sliderInput("WCC","autocorrelation:", min=0,max=1,step=0.005,value=0.9) ,
+ sidebarPanel(
+   
+    sliderInput("WCC","autocorrelation:", min=0,max=1,step=0.005,value=c(0.5,0.7)) ,
     
+    selectInput("whichscale", label = ("Which scale would you like to use for ICC?"), 
+                choices = list("0-0.1" = 1,"0.1-1" = 2), selected = 1),
     
-    sliderInput("rho","intra-correlation:", min=0,max=0.1,step=0.005,value=0.01) ) ,
+    #only show this scale if scale1 chosen
+    conditionalPanel(
+      condition = "input.whichscale == '1'",
+        sliderInput("rho1","intra-correlation:", min=0,max=0.1,step=0.001,value=0.01)
+                ),
+    #only show this scale if scale2 chosen
+    conditionalPanel(
+      condition = "input.whichscale == '2'",
+      sliderInput("rho2","intra-correlation:", min=0.1,max=1,step=0.01,value=0.1)
+                )
+    ,
   
-  actionButton('reset', 'Clear file'),
-  numericInput("n",
-               "Number of subjects in each cluster-period, n:",
-               min = 10,
-               max=1000,
-               step = 10,
-               value = 200),
+  # Create table output
+  #https://stackoverflow.com/questions/38372906/constrain-two-sliderinput-in-shiny-to-sum-to-100
+  #mainPanel(tableOutput("restable")),
   
-  numericInput("ytop",
-               "Size of y-axis:",
-               min = 0,
-               max=10,
-               step = 1,
-               value = 2),
+  #actionButton('reset', 'Clear file'),
+  
   
   ## adding select option
   ######################
@@ -47,5 +54,39 @@ ui <- fluidPage(
               choices = list("prospective baseline data collection" = 1, 
                              "retrospective baseline data collection" = 2), selected = 1),
   
-  mainPanel(plotOutput("plot2"))
+  
+  #only show this scale if prospective data chosen
+  conditionalPanel(
+    condition = "input.select == '1'",
+    numericInput("n",
+                 "total cluster size (baseline plus endline)",
+                 min = 10,
+                 max=1000,
+                 step = 10,
+                 value = 200)
+    ),
+  #only show this scale if retrospective data chosen
+  conditionalPanel(
+    condition = "input.select == '2'",
+    numericInput("n_retro",
+                 "cluster size - endline data",
+                 min = 10,
+                 max=1000,
+                 step = 10,
+                 value=200)
+  ),
+  numericInput("ytop",
+               "Size of y-axis:",
+               min = 0,
+               max=10,
+               step = 0.1,
+               value = 2)
+ ),  
+#for debugging
+#  verbatimTextOutput("value"),
+# verbatimTextOutput("range"),
+  
+mainPanel(plotOutput("plot2"), textOutput("text_calc"))
+
+##mainPanel(tableOutput("restable"))
 )
