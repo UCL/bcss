62d6ca24bed4731254afe8d2825533ed1618e42b
KMcgrathUCL <kevin.mcgrath@ucl.c.uk>
2019-02-25 15:38:26 +0000

    Update legend on plots

--- server.R
+++ server.R
@@ -30,34 +30,40 @@ server <- function(input,output,session){
   #try this for proportion it works!
   R2eqprop9<-function(x){ (1-input$rho2+(input$n*input$rho2*(1-x)))*(1/(1-x))*(1-((input$WCC[1]*input$WCC[1]*input$rho2*input$rho2*input$n*input$n*x*(1-x))/((1+(((input$n*(1-x))-1)*input$rho2))*(1+(((input$n*x)-1)*input$rho2)))))/(1+((input$n-1)*input$rho2)) }
   R2eqprop<-function(x){ (1-input$rho2+(input$n*input$rho2*(1-x)))*(1/(1-x))*(1-((input$WCC[2]*input$WCC[2]*input$rho2*input$rho2*input$n*input$n*x*(1-x))/((1+(((input$n*(1-x))-1)*input$rho2))*(1+(((input$n*x)-1)*input$rho2)))))/(1+((input$n-1)*input$rho2)) }
-  
-  
  
+   
+  
+  
   #output$value <-renderPrint({input$rho1}) 
   output$range <- renderPrint({ input$WCC })
   
   # prospective is select==1
-  output$plot2 <- renderPlot({ if ( (input$select=="1")   & (input$whichscale=="1") ) { 
-    ggplot(data.frame(x=c(0,1)),aes(x=x))+ggtitle(paste("size=",input$n," ICC=",input$rho1," WCC=",input$WCC[1]," ",input$WCC[2]))+
+  output$plot2 <- renderPlot(
+   { legends=paste0("pi= ",input$WCC)
+   if ( (input$select=="1")   & (input$whichscale=="1") ) {  
+      ggplot(data.frame(x=c(0,1)),aes(x=x,colour=legends))+ggtitle(paste("size=",input$n," ICC=",input$rho1," pi=",input$WCC[1]," ",input$WCC[2]))+
       theme(plot.title = element_text(face="plain",size=12),panel.background=element_blank(),axis.line = element_line(colour="black")) +xlab("Baseline data - proportion of total") + ylab("Proportionate change in clusters required") + 
-      stat_function(fun=eqprop,geom="line",aes(colour="high WCC") )+stat_function(fun=eqprop9,geom="line",aes(colour="low WCC") )  +
-      coord_cartesian(ylim=c(0,input$ytop))  + scale_colour_manual("", values = c("red", "blue")) }                       
+      stat_function(fun=eqprop,geom="line",aes(colour=legends) )+stat_function(fun=eqprop9,geom="line",aes(colour=legends),colour="red" )  +
+      coord_cartesian(ylim=c(0.9,input$ytop))  + scale_colour_manual("", values = c("red", "blue")) } #+geom_point(data=opt_dat,size=4 ) }                       
     else if (( input$select=="1") & (input$whichscale=="2") ) { 
-    ggplot(data.frame(x=c(0,1)),aes(x=x))+ggtitle(paste("size=",input$n," ICC=",input$rho2," WCC=",input$WCC[1]," ",input$WCC[2]))+
+    ggplot(data.frame(x=c(0,1)),aes(x=x,colour=legends))+ggtitle(paste("size=",input$n," ICC=",input$rho2," pi=",input$WCC[1]," ",input$WCC[2]))+
       theme(plot.title = element_text(face="plain",size=12),panel.background=element_blank(),axis.line = element_line(colour="black")) +xlab("Baseline data - proportion of total") + ylab("Proportionate change in clusters required") + 
-      stat_function(fun=R2eqprop,geom="line",aes(colour="high WCC" ))+stat_function(fun=R2eqprop9,geom="line",aes(colour="low WCC") )  +
+      stat_function(fun=R2eqprop,geom="line",aes(colour=legends ))+stat_function(fun=R2eqprop9,geom="line",colour="red",aes(colour=legends) )  +
       coord_cartesian(ylim=c(0,input$ytop)) + scale_colour_manual("", values = c("red", "blue")) }                       
     else if ((input$select=="2") & (input$whichscale=="1")) { 
-      ggplot(data.frame(x=c(0,1)),aes(x=x))+ggtitle(paste("size =",input$n_retro,"ICC = ",input$rho1,"WCC=",input$WCC[1]," ",input$WCC[2]))+
-        theme(plot.title = element_text(face="plain"),panel.background=element_blank(),axis.line = element_line(colour="black"))+ xlab("Baseline data - ratio to main trial data") + ylab("Proportionate change in clusters required") + stat_function(fun=eq,geom="line",aes(colour="high WCC")  )+stat_function(fun=eq9,geom="line",aes(colour="low WCC"))+
+      ggplot(data.frame(x=c(0,1)),aes(x=x,colour=legends))+ggtitle(paste("size =",input$n_retro,"ICC = ",input$rho1,"pi=",input$WCC[1]," ",input$WCC[2]))+
+        theme(plot.title = element_text(face="plain"),panel.background=element_blank(),axis.line = element_line(colour="black"))+ xlab("Baseline data - ratio to main trial data") + ylab("Proportionate change in clusters required") + stat_function(fun=eq,geom="line",aes(colour=legends)  )+stat_function(fun=eq9,geom="line",colour="green",aes(colour=legends))+
         coord_cartesian(ylim=c(0,input$ytop)) + scale_colour_manual("", values = c("green", "orange")) }
     else if ((input$select=="2") & (input$whichscale=="2")) {
-      ggplot(data.frame(x=c(0,1)),aes(x=x))+ggtitle(paste("size =",input$n_retro,"ICC = ",input$rho2,"WCC=",input$WCC[1]," ",input$WCC[2]))+
-        theme(plot.title = element_text(face="plain"),panel.background=element_blank(),axis.line = element_line(colour="black"))+ xlab("Baseline data - ratio to main trial data") + ylab("Proportionate change in clusters required") + stat_function(fun=R2eq,geom="line",aes(colour="high WCC") )+stat_function(fun=R2eq9,geom="line",aes(colour="low WCC"))+
+      ggplot(data.frame(x=c(0,1)),aes(x=x,colour=legends))+ggtitle(paste("size =",input$n_retro,"ICC = ",input$rho2,"pi=",input$WCC[1]," ",input$WCC[2]))+
+        theme(plot.title = element_text(face="plain"),panel.background=element_blank(),axis.line = element_line(colour="black"))+ xlab("Baseline data - ratio to main trial data") + ylab("Proportionate change in clusters required") + stat_function(fun=R2eq,geom="line",aes(colour=legends) )+stat_function(fun=R2eq9,geom="line",colour="green",aes(colour=legends))+
         coord_cartesian(ylim=c(0,input$ytop)) + scale_colour_manual("", values = c("green", "orange")) }
     }
     ) 
 
+
+  
+  
 # for prospective, calculate optimal value of x, try 
 # https://stackoverflow.com/questions/40997817/reactive-variables-in-shiny-for-later-calculations
 
@@ -75,7 +81,7 @@ server <- function(input,output,session){
      #paste("Optimal (x,y) = (", opt_x2,",",opt_y2," ),")  }
      #paste("Optimal (x,y) = (", round((n*rho1*WCC1+rho1-1)/(rho1*n*(1+WCC1)),digits=4),",", round(eqprop(n*rho1*WCC1+rho1-1)/(rho1*n*(1+WCC1)),digits=4),")  ")  
     #paste("Optimal (x,y) = (", round((n*rho1*WCC2+rho1-1)/(rho1*n*(1+WCC2)),digits=4),",", round(eqprop9(n*rho1*WCC2+rho1-1)/(rho1*n*(1+WCC2)),digits=4),")  ") } 
-    else if (( input$select=="1") & ( input$whichscale=="2") ) {
+     else if (( input$select=="1") & ( input$whichscale=="2") ) {
       n <-input$n
       rho2 <-input$rho2
       WCC1 <- input$WCC[1]
@@ -86,9 +92,12 @@ server <- function(input,output,session){
       opt_y2 <- round(R2eqprop(opt_x2),digits=4)
       paste("Optimal (x,y) = (", opt_x,",",opt_y," ), (", opt_x2,",",opt_y2," )")} 
       #paste("Optimal (x,y) = (", round((n*rho2*WCC+rho2-1)/(rho2*n*(1+WCC)),digits=4),",", round(eqprop(n*rho2*WCC+rho2-1)/(rho2*n*(1+WCC)),digits=4),")  ")  }
-  }) 
-   
-}
+  }
+  #render end below
+ 
+   ) 
+  
+ }
         
   
 #https://stackoverflow.com/questions/38917101/how-do-i-show-the-y-value-on-tooltip-while-hover-in-ggplot2
@@ -102,6 +111,4 @@ server <- function(input,output,session){
   
 #https://gitlab.com/snippets/16220
   
- 
-
 
