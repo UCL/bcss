357a79c23a8fdabe9e664c1defc68d1278800bae
KMcgrathUCL <45361270+KMcgrathUCL@users.noreply.github.com>
2019-04-09 12:23:50 +0100

    update server.R
    plotting optimal points on curve

--- server.R
+++ server.R
@@ -35,19 +35,40 @@ server <- function(input,output,session){
   #output$value <-renderPrint({input$rho1}) 
   output$range <- renderPrint({ input$WCC })
   
+
+  
+  
   # prospective is select==1
   output$plot2 <- renderPlot(
    { legends=paste0("pi= ",input$WCC)
    if ( (input$select=="1")   & (input$whichscale=="1") ) {  
-      ggplot(data.frame(x=c(0,1)),aes(x=x,colour=legends))+ggtitle(paste("size=",input$n," ICC=",input$rho1," pi=",input$WCC[1]," ",input$WCC[2]))+
+     n <-input$n
+     rho1 <-input$rho1
+     WCC1 <- input$WCC[1]
+     WCC2 <- input$WCC[2]
+     opt_x <- round((n*rho1*WCC1+rho1-1)/(rho1*n*(1+WCC1)),digits=4)
+     opt_y <- round(eqprop9(opt_x),digits=4)
+     opt_x2 <- round((n*rho1*WCC2+rho1-1)/(rho1*n*(1+WCC2)),digits=4)
+     opt_y2 <- round(eqprop(opt_x2),digits=4)
+     nedf2 <- data.frame("x"=c(max(0,opt_x),max(0,opt_x2)),"y"=c(opt_y,opt_y2))
+     ggplot(data.frame(x=c(0,1)),aes(x=x,colour=legends))+ggtitle(paste("size=",input$n," ICC=",input$rho1," pi=",input$WCC[1]," ",input$WCC[2]))+
       theme(plot.title = element_text(face="plain",size=12),panel.background=element_blank(),axis.line = element_line(colour="black")) +xlab("Baseline data - proportion of total") + ylab("Proportionate change in clusters required") + 
       stat_function(fun=eqprop,geom="line",aes(colour=legends) )+stat_function(fun=eqprop9,geom="line",aes(colour=legends),colour="red" )  +
-      coord_cartesian(ylim=c(0.2,input$ytop))  + scale_colour_manual("", values = c("red", "blue")) } #+geom_point(data=opt_dat,size=4 ) }                       
+      coord_cartesian(ylim=c(0.2,input$ytop))  + scale_colour_manual("", values = c("red", "blue")) +geom_point(data=nedf2,aes(x,y)) }                       
     else if (( input$select=="1") & (input$whichscale=="2") ) { 
+      n <-input$n
+      rho2 <-input$rho2
+      WCC1 <- input$WCC[1]
+      WCC2 <- input$WCC[2]
+      opt_x <- round((n*rho2*WCC1+rho2-1)/(rho2*n*(1+WCC1)),digits=4)
+      opt_y <- round(R2eqprop9(opt_x),digits=4)
+      opt_x2 <- round((n*rho2*WCC2+rho2-1)/(rho2*n*(1+WCC2)),digits=4)
+      opt_y2 <- round(R2eqprop(opt_x2),digits=4)
+      nedf2 <- data.frame("x"=c(max(0,opt_x),max(0,opt_x2)),"y"=c(opt_y,opt_y2))
     ggplot(data.frame(x=c(0,1)),aes(x=x,colour=legends))+ggtitle(paste("size=",input$n," ICC=",input$rho2," pi=",input$WCC[1]," ",input$WCC[2]))+
       theme(plot.title = element_text(face="plain",size=12),panel.background=element_blank(),axis.line = element_line(colour="black")) +xlab("Baseline data - proportion of total") + ylab("Proportionate change in clusters required") + 
       stat_function(fun=R2eqprop,geom="line",aes(colour=legends ))+stat_function(fun=R2eqprop9,geom="line",colour="red",aes(colour=legends) )  +
-      coord_cartesian(ylim=c(0,input$ytop)) + scale_colour_manual("", values = c("red", "blue")) }                       
+      coord_cartesian(ylim=c(0,input$ytop)) + scale_colour_manual("", values = c("red", "blue")) +geom_point(data=nedf2,aes(x,y)) }                       
     else if ((input$select=="2") & (input$whichscale=="1")) { 
       ggplot(data.frame(x=c(0,1)),aes(x=x,colour=legends))+ggtitle(paste("size =",input$n_retro,"ICC = ",input$rho1,"pi=",input$WCC[1]," ",input$WCC[2]))+
         theme(plot.title = element_text(face="plain"),panel.background=element_blank(),axis.line = element_line(colour="black"))+ xlab("Baseline data - ratio to main trial data") + ylab("Proportionate change in clusters required") + stat_function(fun=eq,geom="line",aes(colour=legends)  )+stat_function(fun=eq9,geom="line",colour="green",aes(colour=legends))+
@@ -65,7 +86,7 @@ server <- function(input,output,session){
 # for prospective, calculate optimal value of x, try 
 # https://stackoverflow.com/questions/40997817/reactive-variables-in-shiny-for-later-calculations
 
-  #output the optimal point for pospective data , select = 1
+  #output the optimal point for prospective data , select = 1
   output$text_calc <- renderText({  if (( input$select=="1") & ( input$whichscale=="1") )  {
      n <-input$n
      rho1 <-input$rho1
@@ -75,6 +96,7 @@ server <- function(input,output,session){
      opt_y <- round(eqprop9(opt_x),digits=4)
      opt_x2 <- round((n*rho1*WCC2+rho1-1)/(rho1*n*(1+WCC2)),digits=4)
      opt_y2 <- round(eqprop(opt_x2),digits=4)
+     nedf2 <- data.frame("x"=c(opt_x,opt_x2),"y"=c(opt_y,opt_y2))
      paste("Optimal (x,y) = (", opt_x,",",opt_y," ), (", opt_x2,",",opt_y2," )")}   
      #paste("Optimal (x,y) = (", opt_x2,",",opt_y2," ),")  }
      #paste("Optimal (x,y) = (", round((n*rho1*WCC1+rho1-1)/(rho1*n*(1+WCC1)),digits=4),",", round(eqprop(n*rho1*WCC1+rho1-1)/(rho1*n*(1+WCC1)),digits=4),")  ")  
@@ -88,14 +110,20 @@ server <- function(input,output,session){
       opt_y <- round(R2eqprop9(opt_x),digits=4)
       opt_x2 <- round((n*rho2*WCC2+rho2-1)/(rho2*n*(1+WCC2)),digits=4)
       opt_y2 <- round(R2eqprop(opt_x2),digits=4)
+      nedf2 <- data.frame("x"=c(opt_x,opt_x2),"y"=c(opt_y,opt_y2))
       paste("Optimal (x,y) = (", opt_x,",",opt_y," ), (", opt_x2,",",opt_y2," )")} 
       #paste("Optimal (x,y) = (", round((n*rho2*WCC+rho2-1)/(rho2*n*(1+WCC)),digits=4),",", round(eqprop(n*rho2*WCC+rho2-1)/(rho2*n*(1+WCC)),digits=4),")  ")  }
   }
   #render end below
+ )
  
-   ) 
   
- }
+
+   
+}  
+
+  
+ 
         
   
 #https://stackoverflow.com/questions/38917101/how-do-i-show-the-y-value-on-tooltip-while-hover-in-ggplot2
