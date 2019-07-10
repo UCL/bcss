4fcfae1978bd8e999a4ee6a04f40d2fb4d9b960d
KMcgrathUCL <kevin.mcgrath@ucl.c.uk>
2019-07-10 14:42:03 +0100

    Update RBaseline.R
    minor edits R standalone program

--- R programs/RBaseline.R
+++ R programs/RBaseline.R
@@ -1,18 +1,18 @@
 ############################################################################################
 # R program converted from Shiny to output static report from the baseline data Shiny app
 # K McGrath Jan 2019
-# updated 25 Feb 2019 
-# updated  8 Apr 2019
+# updated  10 Jul 2019
 ############################################################################################
 
 
 library(ggplot2)
 # input variables
 
+# in order to select prospective or retrospective
 # hard code input values here,  select=1 for prospective, 2 for retrospective plots
 # comment out respective line
 
-input <- data.frame(rho=0.01, WCC1=0.5,WCC2=0.9,n=200, select=1,ytop=1.25,ybot=0.5)
+input <- data.frame(rho=0.01, WCC1=0.5,WCC2=0.9,n=200, select=1,ytop=1.25,ybot=0.4)
 #input <- data.frame(rho=0.1, WCC1=0.5,WCC2=0.9,n_retro=200,select=2,ytop=1.25,ybot=0)
 
 # eq,eq9 for retrospective 
@@ -36,15 +36,15 @@ pi=c(input$WCC1,input$WCC2)
 legends=paste0("pi= ",pi)
 # prospective select =1 
 plot2 <- ({ if ( (input$select=="1") ) { 
-  ggplot(data.frame(x=c(0,1)),aes(x=x,colour=legends))+ggtitle(label=paste("size=",input$n," ICC=",input$rho," pi=",input$WCC1," ",input$WCC2),subtitle=paste("opt(x,y)=  (",Lopt_x,",",Lopt_y,")  (",Hopt_x,",",Hopt_y,")" ))+ 
-    theme(plot.title = element_text(face="plain",size=12,colour = "black"),plot.subtitle = element_text(colour = "red"),legend.position = "top",panel.background=element_blank(),axis.line = element_line(colour="black")) +xlab("Baseline data - proportion of total") + ylab("Proportionate change in clusters required") + #stat_function(fun=eq,geom="line",colour="red"  )+stat_function(fun=eq9,geom="line",colour="blue")+
+  ggplot(data.frame(x=c(0,0.5)),aes(x=x,colour=legends))+ggtitle(label=paste("total cluster size=",input$n," ICC=",input$rho))+ 
+    theme(plot.title = element_text(face="plain",size=12,colour = "black"),plot.subtitle = element_text(colour = "red"),legend.position = "top",panel.background=element_blank(),axis.line = element_line(colour="black")) +xlab("Baseline data as proportion of total") + ylab("Proportionate change in clusters required") + #stat_function(fun=eq,geom="line",colour="red"  )+stat_function(fun=eq9,geom="line",colour="blue")+
     stat_function(fun=eqprop,geom="line",aes(colour=legends) )+stat_function(fun=eqprop9,geom="line",aes(colour=legends),colour="red" )  +
     coord_cartesian(ylim=c(input$ybot,input$ytop)) + scale_colour_manual("", values = c("red", "blue")) +geom_point(data=nedf,aes(x,y)) 
     }                      
 # retrospective select = 2
    else if ((input$select=="2") ) { 
-    ggplot(data.frame(x=c(0,1)),aes(x=x,colour=legends))+ggtitle(label=paste("size =",input$n_retro,"ICC = ",input$rho,"pi=",input$WCC1," ",input$WCC2))+
-      theme(plot.title = element_text(face="plain",size=12,colour="black"),plot.subtitle = element_text(colour = "red"),legend.position = "top",panel.background=element_blank(),axis.line = element_line(colour="black"))+ xlab("Baseline data - ratio to main trial data") + ylab("Proportionate change in clusters required") + 
+    ggplot(data.frame(x=c(0,1)),aes(x=x,colour=legends))+ggtitle(label=paste("endline cluster size =",input$n_retro,"ICC = ",input$rho,"pi=",input$WCC1," ",input$WCC2))+
+      theme(plot.title = element_text(face="plain",size=12,colour="black"),plot.subtitle = element_text(colour = "red"),legend.position = "top",panel.background=element_blank(),axis.line = element_line(colour="black"))+ xlab("Baseline data as a ratio to endline") + ylab("Proportionate change in clusters required") + 
        stat_function(fun=eq,geom="line",aes(colour=legends) )+stat_function(fun=eq9,geom="line",aes(colour=legends),colour="green")+
       # stat_function(fun=eqprop,geom="line",colour="low WCC")+stat_function(fun=eqprop9,geom="line",aes(colour="high WCC"))  +
       coord_cartesian(ylim=c(input$ybot,input$ytop)) + scale_colour_manual("", values = c("green", "orange")) }
@@ -73,6 +73,13 @@ if (( input$select=="1")  )  {
   Hopt_x <-round((n*rho*WCC2+rho-1)/(rho*n*(1+WCC2)),digits=4)
   Hopt_y<-round(eqpropf(Hopt_x,input$WCC2),digits=4)   
 
+ #If any of the x values are negative in order to prevent the plots showing negative x scale assign all the optimal x's and y's to zero.
+  if ( Lopt_x<0 | Lopt_y <0 ) { 
+    Lopt_x =0
+    Lopt_y=0
+    Hopt_x=0
+    Hopt_y=0 }
+  
 #need to create data set to plot opt points on ggplot,as parameter to geom_point 
 nedf <- data.frame("x"=c(Lopt_x,Hopt_x),"y"=c(Lopt_y,Hopt_y))
 }
