48747eb6c6896027b13d5f4816cf93672b2ed865
KMcgrathUCL <kevin.mcgrath@ucl.c.uk>
2019-07-10 15:10:02 +0100

    ui update 10 Jul 2019

--- R programs/ui.R
+++ R programs/ui.R
@@ -1,9 +1,8 @@
-# 
 ####################################################################################
-# R (Shiny) prog to plot interactive chart for cRCTs with varying baseline data
+# R (Shiny)   user-interface program
+# prog to plot interactive chart for cRCTs with varying baseline data
 # R shiny app to accompany \"Cluster randomised trials with baseline data:sample size and optimal designs\" ,Copas and Hooper
 # Author: Kevin McGrath
-# update 10 July 2019 
 ####################################################################################
 
 
@@ -14,75 +13,37 @@
 
 library(shiny)
 library(ggplot2)
-
+#library(plotly)
+#library(shinyjs)
 ui <- fluidPage(
   titlePanel(title = h4("R shiny app to accompany \"Cluster randomised trials with baseline data:sample size and optimal designs\" ,Copas and Hooper")),
   sidebarPanel(
-    
-    sliderInput("WCC","autocorrelation:", min=0,max=1,step=0.005,value=c(0.5,0.7)) ,
-    
-    selectInput("whichscale", label = ("Which scale would you like to use for ICC?"), 
-                choices = list("0-0.1" = 1,"0.1-1" = 2), selected = 1),
-    
-    #only show this scale if scale1 chosen
-    conditionalPanel(
-      condition = "input.whichscale == '1'",
-      sliderInput("rho1","intra-correlation:", min=0,max=0.1,step=0.001,value=0.01)
-    ),
-    #only show this scale if scale2 chosen
-    conditionalPanel(
-      condition = "input.whichscale == '2'",
-      sliderInput("rho2","intra-correlation:", min=0.1,max=1,step=0.01,value=0.1)
-    ),
+    sliderInput("WCC","autocorrelation:", min=0,max=1,step=0.005,value=0.9) ,
     
     
-    # Create table output
-    #https://stackoverflow.com/questions/38372906/constrain-two-sliderinput-in-shiny-to-sum-to-100
-    #mainPanel(tableOutput("restable")),
-    
-    #actionButton('reset', 'Clear file'),
-    
-    
-    ## adding select option
-    ######################
-    #conditionalPanel(condition="input.conditionedPanels==1",
-    selectInput("select", label = ("What would you like to calculate?"), 
-                choices = list("prospective baseline data collection" = 1, 
-                               "retrospective baseline data collection" = 2), selected = 1),
-    
-    
-    #only show this scale if prospective data chosen
-    conditionalPanel(
-      condition = "input.select == '1'",
-      numericInput("n",
-                   "total cluster size (baseline plus endline)",
-                   min = 10,
-                   max=1000,
-                   step = 10,
-                   value = 200),
-      numericInput("ytop",
-                   "Size of y-axis:",
-                   min = 0,
-                   max=10,
-                   step = 0.1,
-                   value = 2)),
-    
-    #only show this scale if retrospective data chosen
-    conditionalPanel(
-      condition = "input.select == '2'",
-      numericInput("n_retro",
-                   "cluster size - endline data",
-                   min = 10,
-                   max=1000,
-                   step = 10,
-                   value=200)  ) ),
-  #only show this scale if prospective data chosen
+    sliderInput("rho","intra-correlation:", min=0,max=0.1,step=0.005,value=0.01) ) ,
   
-  #for debugging
-  #  verbatimTextOutput("value"),
-  # verbatimTextOutput("range"),
+  actionButton('reset', 'Clear file'),
+  numericInput("n",
+               "Number of subjects in each cluster-period, n:",
+               min = 10,
+               max=1000,
+               step = 10,
+               value = 200),
   
-  mainPanel(plotOutput("plot2"), textOutput("text_calc"))
+  numericInput("ytop",
+               "Size of y-axis:",
+               min = 0,
+               max=10,
+               step = 1,
+               value = 2),
   
-  ##mainPanel(tableOutput("restable"))
+  ## adding select option
+  ######################
+  #conditionalPanel(condition="input.conditionedPanels==1",
+  selectInput("select", label = ("What would you like to calculate?"), 
+              choices = list("prospective baseline data collection" = 1, 
+                             "retrospective baseline data collection" = 2), selected = 1),
+  
+  mainPanel(plotOutput("plot2"))
 )
